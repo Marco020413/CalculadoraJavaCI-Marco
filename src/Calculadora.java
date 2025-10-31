0ba55ddcff345d264f4291daa022e00d7fb04eaf
Marco020413 <marf1382003@gmail.com>
2025-10-30 23:13:27 -0600

    Initial commit for Java Calculator project

---
+++ src/Calculadora.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Calculadora {     public static int sumar(int a, int b) {         return a + b; 
+    } 
+ 
+    public static int restar(int a, int b) {         return a - b; 
+    } 
+ 
+    public static int multiplicar(int a, int b) {         return a * b; 
+    } 
+ 
+    public static int dividir(int a, int b) { 
+        if (b == 0) throw new ArithmeticException("No se puede dividir entre cero");         return a / b; 
+    } 
+} 
+ 
