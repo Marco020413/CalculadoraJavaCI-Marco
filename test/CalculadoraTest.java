55ae6e680bd50703357090647d5b503b0493b3db
Marco020413 <marf1382003@gmail.com>
2025-11-06 22:32:07 -0600

    Pr_2

--- test/CalculadoraTest.java
+++ test/CalculadoraTest.java
@@ -3,7 +3,7 @@ import src.Calculadora;
 
 public class CalculadoraTest {     public static void main(String[] args)
      {
-      assert Calculadora.sumar(2, 3) == 5 : "Error en suma";        
+      assert Calculadora.sumar(2, 3) == 100 : "Error en suma";        
       assert Calculadora.restar(5, 3) == 2 : "Error en resta";        
       assert Calculadora.multiplicar(2, 4) == 8 : "Error en multiplicación";         
       assert Calculadora.dividir(10, 2) == 5 : "Error en división"; 
